211d932aa2bb9434c55fc86bf65e03c31f4d8334
veronese_d <daniel.veronese@iisviolamarchesini.edu.it>
2022-05-12 21:58:12 +0200

    .

--- prova/App.js
+++ prova/App.js
@@ -1,27 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View ,TextInput} from 'react-native';
-import { Button } from 'react-native-web';
+import { StyleSheet, Text, View, TextInput,Button } from 'react-native';
 
 export default function App() {
   return (
-    <View >
+    <View style={{padding:50,marginTop:50,flexDirection:"row",alignItems:"center"}}>
+        <TextInput placeholder='srivi todo' style={styles.input} />
+
+        <Button title='invia' onPress={() =>  {}} />
+      
+          
+
     
-         
-
-         
-
-
-
- 
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+input:{
+
+with:'100%',
+borderWidth:1,
+
+}
 });
