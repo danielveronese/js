3e43eb8dbba735848f4fc577eb2f1233947e1faa
veronese_d <daniel.veronese@iisviolamarchesini.edu.it>
2022-05-12 18:38:53 +0200

    .

---
+++ prova/App.js
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View ,TextInput} from 'react-native';
+import { Button } from 'react-native-web';
+
+export default function App() {
+  return (
+    <View >
+    
+         
+
+         
+
+
+
+ 
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
