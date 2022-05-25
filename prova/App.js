866a711206819bf764bb86b002d4d715627387ea
veronese_d <daniel.veronese@iisviolamarchesini.edu.it>
2022-05-25 08:29:47 +0200

    .

--- prova/App.js
+++ prova/App.js
@@ -1,25 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput,Button } from 'react-native';
+import * as React from 'react';
+import { Text, View, StyleSheet,TextInput,Button } from 'react-native';
+import Constants from 'expo-constants';
 
-export default function App() {
+// You can import from local files
+import AssetExample from './components/AssetExample';
+
+// or any pure javascript modules available in npm
+import { Card } from 'react-native-paper';
+
+export default class App extends React.Component {
+  state = {
+     
+    value:'',
+    todolist: [],
+
+  }
+    onChangeTextHandler=(text)=> {
+     this.setState({value: text})
+
+    }
+    addtodohandler=() =>{
+
+       this.setState(prevState =>{
+         return{
+
+         todolist: prevState.todolist.concat(prevState.value)
+         }
+       })
+
+    }
+    render(){
+      const list = this.state.todolist.map(todo=>{
+      return<Text>{todo}</Text>
+         
+      })
   return (
-    <View style={{padding:50,marginTop:50,flexDirection:"row",alignItems:"center"}}>
-        <TextInput placeholder='srivi todo' style={styles.input} />
-
-        <Button title='invia' onPress={() =>  {}} />
-      
-          
-
-    
+    <View >
+      <View style={{padding:50,marginTop:50,flexDirection:'row',alignItems:'center',}}>
+           <TextInput value={this.state.value} onChangeText={this.onChangeTextHandler} placeholder='crivi todo' style={styles.input} />
+           <Button title='invia' style={styles1.button} onPress={this.addtodohandler}   />
+      </View>
+      <View>
+        {list}
+      </View>
     </View>
   );
 }
+}
 
 const styles = StyleSheet.create({
-input:{
+  input: {
+ width:'80%',
+ borderWidth:1,
+ borderRadius:5,
+  },
 
-with:'100%',
-borderWidth:1,
 
-}
 });
+const styles1 = StyleSheet.create({
+    button:{
+  borderRadius:20,
+    },
+  });
+
